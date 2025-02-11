cc9f43602e8452e86379cd86902c11af14d05731
wjsrlgns <a7s8d9789@naver.com>
2025-02-12 02:02:25 +0900

    section3 작업

--- src/pages/main/Section3.js
+++ src/pages/main/Section3.js
@@ -1,33 +1,58 @@
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper";
+import SwiperCore, { Navigation, Pagination } from "swiper";
 import "swiper/swiper-bundle.min.css";
 import "../../css/Section3.css";
 
 function Section3() {
+  SwiperCore.use([Navigation, Pagination]);
+
   return (
     <div className="Businesses">
       <h3>Core Businesses</h3>
       <span>전국을 잇는 철도</span>
       <p>코레일은 철도 운송을 중심으로 다양한 사업을 통해 대한민국의 교통 혁신을 선도하고 있습니다. </p>
-      <Swiper modules={[Navigation, Pagination]} spaceBetween={50} slidesPerView={1} navigation={{ clickable: true }}>
-        <SwiperSlide>
-          <div className="swiper_wrap">
-            <img src={require("../../images/train1.jpg")} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiper_wrap">
-            <img src={require("../../images/train1.jpg")} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiper_wrap">
-            <img src={require("../../images/train1.jpg")} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-      </Swiper>
+      <div className="sec3-swiper-wrap">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          slidesPerView={"auto"}
+          spaceBetween={30}
+          centeredSlides={true}
+          loop={true}
+          loopedSlides={2}
+          navigation={{ clickable: true }}>
+          <SwiperSlide>
+            <div className="slide-item">
+              <div className="text">1</div>
+              <img src={require("../../images/train1.jpg")} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slide-item">
+              <div className="text">2</div>
+              <img src={require("../../images/train1.jpg")} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slide-item">
+              <div className="text">3</div>
+              <img src={require("../../images/train1.jpg")} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slide-item">
+              <div className="text">4</div>
+              <img src={require("../../images/train1.jpg")} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slide-item">
+              <div className="text">5</div>
+              <img src={require("../../images/train1.jpg")} alt="" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
