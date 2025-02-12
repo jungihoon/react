36163c1d94e7806eeb9a1a21f6205276d4cccf4e
jgh <a7s8d9789@naver.com>
2025-02-12 14:43:23 +0900

    section3 작업

--- src/pages/main/Section3.js
+++ src/pages/main/Section3.js
@@ -10,44 +10,66 @@ function Section3() {
   return (
     <div className="Businesses">
       <h3>Core Businesses</h3>
-      <span>전국을 잇는 철도</span>
-      <p>코레일은 철도 운송을 중심으로 다양한 사업을 통해 대한민국의 교통 혁신을 선도하고 있습니다. </p>
+      <div className="Businesses_txt">
+        <span>전국을 잇는 철도</span>
+        <p>코레일은 철도 운송을 중심으로 다양한 사업을 통해 대한민국의 교통 혁신을 선도하고 있습니다. </p>
+      </div>
       <div className="sec3-swiper-wrap">
         <Swiper
           modules={[Navigation, Pagination]}
           slidesPerView={"auto"}
           spaceBetween={30}
-          centeredSlides={true}
           loop={true}
           loopedSlides={2}
           navigation={{ clickable: true }}>
           <SwiperSlide>
             <div className="slide-item">
-              <div className="text">1</div>
+              <div className="text">
+                ㅋ
+                <span>
+                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ <br />
+                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                </span>
+              </div>
               <img src={require("../../images/train1.jpg")} alt="" />
             </div>
           </SwiperSlide>
+
           <SwiperSlide>
             <div className="slide-item">
-              <div className="text">2</div>
+              <div className="text">
+                ㅋㅋ
+                <span>
+                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ <br />
+                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                </span>
+              </div>
               <img src={require("../../images/train1.jpg")} alt="" />
             </div>
           </SwiperSlide>
+
           <SwiperSlide>
             <div className="slide-item">
-              <div className="text">3</div>
+              <div className="text">
+                ㅋㅋㅋ
+                <span>
+                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ <br />
+                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                </span>
+              </div>
               <img src={require("../../images/train1.jpg")} alt="" />
             </div>
           </SwiperSlide>
+
           <SwiperSlide>
             <div className="slide-item">
-              <div className="text">4</div>
-              <img src={require("../../images/train1.jpg")} alt="" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slide-item">
-              <div className="text">5</div>
+              <div className="text">
+                ㅋㅋㅋㅋ
+                <span>
+                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ <br />
+                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                </span>
+              </div>
               <img src={require("../../images/train1.jpg")} alt="" />
             </div>
           </SwiperSlide>
