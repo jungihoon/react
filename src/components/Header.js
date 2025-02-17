03b714a3a50444b48e36c70f6f79b4e5bf097afb
jgh <a7s8d9789@naver.com>
2025-02-17 18:07:50 +0900

    sec3반응형 작업 종료

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,22 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import img1 from "../images/logo.png";
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import img1 from '../images/logo.svg';
 
 function Header() {
-  return (
-    <header>
-      <h1>
-        <NavLink to="/">
-          <img src={img1} alt="사이트 로고" />
-        </NavLink>
-      </h1>
-      <nav>
-        <ul>
-          {/* <li>
-            <NavLink to="/ Main">???</NavLink>
-          </li> */}
-          <li>
-            <NavLink to="/Company">공사소개</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Business">주요사업</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Train">열차소개</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Fee">이건먼데</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+    return (
+      <header>
+       <h1><NavLink to="/" ><img src={img1} alt="사이트 로고" /></NavLink></h1>
+       <nav>
+           <ul>
+               <li><NavLink to="/Company">공사소개</NavLink></li>
+               <li><NavLink to="/Business">사업소개</NavLink></li>
+               <li><NavLink to="/Train">열차소개</NavLink></li>
+               <li><NavLink to="/Fee">열차운임표</NavLink></li>
+               <li><NavLink to="/Place">역위치정보</NavLink></li>
+           </ul>
+         </nav>
+       </header>
+    );
+  }
 
-export default Header;
+  export default Header;
--- src/pages/main/Section3.js
+++ src/pages/main/Section3.js
@@ -8,14 +8,29 @@ function Section3() {
   SwiperCore.use([Navigation, Pagination]);
 
   return (
-    <div className="Businesses">
-      <h3>Core Businesses</h3>
+    <section className="Businesses">
+      <h3 className="hidden">사업소개</h3>
+      <p>Core Businesses</p>
       <div className="Businesses_txt">
         <span>전국을 잇는 철도</span>
         <p>코레일은 철도 운송을 중심으로 다양한 사업을 통해 대한민국의 교통 혁신을 선도하고 있습니다. </p>
       </div>
       <div className="sec3-swiper-wrap">
         <Swiper
+          breakpoints={{
+            // 1024: {
+            //   slidesPerView: 2,
+            // },
+            769: {
+              slidesPerView: 1,
+              spaceBetween: 30,
+            },
+
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 400,
+            },
+          }}
           modules={[Navigation, Pagination]}
           slidesPerView={"auto"}
           spaceBetween={30}
@@ -25,10 +40,12 @@ function Section3() {
           <SwiperSlide>
             <div className="slide-item">
               <div className="text">
-                ㅋ
+                여객수송
                 <span>
-                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ <br />
-                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                  한국철도공사 여객사업본부는, 전국을 반나절 생활권으로 <br />
+                  이어주는 고속열차와 지역과 지역을 연결하는 일반열차를
+                  <br />
+                  통해 안전하고 편리한 여객 서비스를 제공합니다.
                 </span>
               </div>
               <img src={require("../../images/train1.jpg")} alt="" />
@@ -38,10 +55,11 @@ function Section3() {
           <SwiperSlide>
             <div className="slide-item">
               <div className="text">
-                ㅋㅋ
+                광역수송
                 <span>
-                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ <br />
-                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                  한국철도공사 광역사업본부는, 광역·도시철도 사업의 국내 최대 <br />
+                  운영자로서 총 15개의 광역철도 노선[경부·경인·경원·장항(1호선), <br />
+                  경의·중앙, 안산·과천, 분당·수인, 경춘, 일산, 경강, 동해, 서해선]을 <br /> 운영하고 있습니다.
                 </span>
               </div>
               <img src={require("../../images/train1.jpg")} alt="" />
@@ -51,23 +69,26 @@ function Section3() {
           <SwiperSlide>
             <div className="slide-item">
               <div className="text">
-                ㅋㅋㅋ
+                물류수송
                 <span>
-                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ <br />
-                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                  한국철도공사 물류사업본부는, 수출입 물량인 컨테이너와
+                  <br /> 국가기간산업에 필요한 시멘트·철강·석탄 등의 운송을 주로 <br />
+                  담당하고 있으며, 기존 운송사업에서 벗어나 하역·창고· <br />
+                  포워딩 등 사업영역 확장을 통해 글로벌 종합물류기업으로의 도약을 <br />
+                  진행하고 있습니다.
                 </span>
               </div>
-              <img src={require("../../images/train1.jpg")} alt="" />
+              <img src={require("../../images/train3.jpg")} alt="" />
             </div>
           </SwiperSlide>
 
           <SwiperSlide>
             <div className="slide-item">
               <div className="text">
-                ㅋㅋㅋㅋ
+                해외사업
                 <span>
-                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ <br />
-                  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                  한국철도공사 해외남북철도사업단은, 철도 기술력과 전문성을 <br /> 바탕으로 해외 철도사업을 수행하고, 글로벌 철도 네트워크를 <br />
+                  구축하여 글로벌 철도를 실현하고 있습니다.
                 </span>
               </div>
               <img src={require("../../images/train1.jpg")} alt="" />
@@ -75,7 +96,7 @@ function Section3() {
           </SwiperSlide>
         </Swiper>
       </div>
-    </div>
+    </section>
   );
 }
 
