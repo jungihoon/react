aa4aef81f9c9ea2ec1b50d86483aef98103dbee0
jgh <a7s8d9789@naver.com>
2025-02-25 18:28:27 +0900

    서브페이지

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import img1 from '../images/logo.svg';
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import img1 from "../images/logo.svg";
 import { FaBars } from "react-icons/fa";
 
 function Header() {
-
   const [upDown, setUpDown] = useState(false); //false(위)/true(아래)
 
   const [isSwitched, setIsSwitced] = useState(false); //햄버거 메뉴 열림(true)/닫힘(false)
@@ -21,38 +20,59 @@ function Header() {
 
   useEffect(() => {
     // window에 scroll 이벤트를 넣는다.
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     // 페이지를 벗어날 때 이벤트를 제거한다. 한번 실행할 때 계속 실행된다.
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-
   const navHandler = (e) => {
     e.preventDefault();
-    setIsSwitced(!isSwitched);  //메뉴 토글
+    setIsSwitced(!isSwitched); //메뉴 토글
   };
 
   return (
-    <header className={upDown ? 'down' : 'up'}>
-      <div className="header_inner">
-        <h1><NavLink to="/" ><img src={img1} alt="코레일 로고" /></NavLink></h1>
-        <nav className={isSwitched ? 'show' : 'hide'}>
-          <ul>
-            <li><NavLink to="/Company">공사소개</NavLink></li>
-            <li><NavLink to="/Business">주요사업</NavLink></li>
-            <li><NavLink to="/Train">열차소개</NavLink></li>
-            <li><NavLink to="/Fee">열차운임표</NavLink></li>
-            <li><NavLink to="/Place">역위치정보</NavLink></li>
-          </ul>
-        </nav>
-        <button onClick={navHandler}>
-          <span className='hidden'>메뉴열기</span>
-          <FaBars className="ico" />
-        </button>
+    <>
+      <div id="skipNav">
+        <a href="#content">본문 바로가기</a>
+        <a href="#gnb">글로벌 네비게이션 바로가기</a>
       </div>
-    </header>
+      <header className={upDown ? "down" : "up"}>
+        <div className="header_inner">
+          <h1>
+            <NavLink to="/">
+              <img src={img1} alt="코레일 로고" />
+              <span className="hidden">코레일</span>
+            </NavLink>
+          </h1>
+          <nav id="gnb" className={isSwitched ? "show" : "hide"}>
+            <h2 className="hidden">글로벌네비게이션</h2>
+            <ul>
+              <li>
+                <NavLink to="/Company">공사소개</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Business">주요사업</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Train">열차소개</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Fee">열차운임표</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Place">역위치정보</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <button onClick={navHandler}>
+            <span className="hidden">메뉴열기</span>
+            <FaBars className="ico" />
+          </button>
+        </div>
+      </header>
+    </>
   );
 }
 
--- src/pages/Company.js
+++ src/pages/Company.js
@@ -1,9 +1,84 @@
-import React from 'react';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import '../css/Company.css'
+import sub1_bg from '../images/sub1/sub1_bg.jpg';
 
-function Company() {
-    return (
-        <h2>공사소개</h2>   
-    );
-  }
+import Management from './sub1/Management';
+import Ceo from './sub1/Ceo';
+import Map from './sub1/Map';
 
-  export default Company;
+
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+
+ function Company(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function ComTab() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+  
+    <Box>
+        {/* 헤드 이미지 추가 */}
+        <Box sx={{ width: '100%', height: '400px', overflow: 'hidden', objectFit: 'cover'  }}>
+        <img 
+          src={sub1_bg} // 원하는 이미지 경로 설정
+          alt="Company Header"
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+        />
+        <h3>공사소개</h3>
+      </Box>
+      <Box sx={{ margin:'100px auto', width: '70%', textAlign:'center'} }>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab sx={{width:'40%',fontSize:'1.2rem', fontWeight:'500'}} label="경영가치체계" {...Company(0)} />
+          <Tab sx={{width:'40%',fontSize:'1.2rem', fontWeight:'500'}}  label="우리들의 이야기" {...Company(1)} />
+          <Tab sx={{width:'40%',fontSize:'1.2rem', fontWeight:'500'}}  label="찾아오시는 길" {...Company(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <Management />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <Ceo />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <Map />
+      </CustomTabPanel>
+    </Box>
+  
+
+  );
+}
